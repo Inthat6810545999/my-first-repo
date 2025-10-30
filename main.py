577290ca3c63c350f0b5e9c6ecc0ba5b5268f52f
Inthat6810545999 <inthat.ni@ku.th>
2025-10-30 15:23:11 +0700

    My first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+
+from bank import BankAccount
+
+def main():
+    account = BankAccount("Alice", 100)
+    print(account)
+    account.deposit(50)
+    account.withdraw(30)
+    print(account)
+
+if __name__ == "__main__":
+    main()
